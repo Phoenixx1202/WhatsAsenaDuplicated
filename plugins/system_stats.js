804fa1d8b0156b3a4c71f17930319ec22a70be96
Phoenixx1202 <mcnet.fenix@gmail.com>
2021-05-27 23:13:53 -0400

    Add files via upload

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -12,6 +12,7 @@ const {MessageType} = require('@adiwajshing/baileys');
 const {spawnSync} = require('child_process');
 const Config = require('../config');
 const chalk = require('chalk');
+const Axios = require('axios');
 
 const Language = require('../language');
 const Lang = Language.getString('system_stats');
@@ -19,13 +20,24 @@ const Lang = Language.getString('system_stats');
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'status', fromMe: true, desc: Lang.STATUS_DESC}, (async (message, match) => {
+    Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
-        if (Config.STATUSMSG == 'default') {
-            await message.client.sendMessage(message.jid,'```Tudo Funcionando!!```\n\n*Versão:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Site:* https://google.com' , MessageType.text);
+        if (Config.ALIVEMSG == 'default') {
+            await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster\n*Plugin Channel:* ' + Config.CHANNEL , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.STATUSMSG + '\n*Powered by WhatsAsena*', MessageType.text);
+            const pow = '*Powered by WhatsAsena*'
+            const payload = Config.ALIVEMSG
+            const status = await message.client.getStatus()
+            const ppUrl = await message.client.getProfilePicture() 
+            const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
+
+            if (!payload.includes('{pp}')) {
+                await message.client.sendMessage(message.jid,payload.replace('{version}', Config.VERSION).replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow, MessageType.text);
+            }
+            else if (payload.includes('{pp}')) {
+                await message.sendMessage(Buffer(resim.data), MessageType.image, { caption: payload.replace('{version}', Config.VERSION).replace('{pp}', '').replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow });
+            }
         }
     }));
 
@@ -39,17 +51,57 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({pattern: 'status', fromMe: false, desc: Lang.STATUS_DESC}, (async (message, match) => {
+    Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
-        if (Config.STATUSMSG == 'default') {
-            await message.client.sendMessage(message.jid,'```Tudo Funcionando!!```\n\n*Versão:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Site:* https://google.com' , MessageType.text);
+        if (Config.ALIVEMSG == 'default') {
+            await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster\n*Plugin Channel:* ' + Config.CHANNEL, MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.STATUSMSG + '\n**', MessageType.text);
+            const pow = '*Powered by WhatsAsena*'
+            const payload = Config.ALIVEMSG
+            const status = await message.client.getStatus()
+            const ppUrl = await message.client.getProfilePicture() 
+            const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
+
+            if (!payload.includes('{pp}')) {
+                await message.client.sendMessage(message.jid,payload.replace('{version}', Config.VERSION).replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow, MessageType.text);
+            }
+            else if (payload.includes('{pp}')) {
+                await message.sendMessage(Buffer(resim.data), MessageType.image, { caption: payload.replace('{version}', Config.VERSION).replace('{pp}', '').replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow });
+            }
         }
     }));
 
     Asena.addCommand({pattern: 'sysd', fromMe: false, desc: Lang.SYSD_DESC}, (async (message, match) => {
+
+        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+        await message.sendMessage(
+            '```' + child + '```', MessageType.text
+        );
+    }));
+
+    Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC, dontAddCommandList: true}, (async (message, match) => {
+
+        if (Config.ALIVEMSG == 'default') {
+            await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster\n*Plugin Channel:* ' + Config.CHANNEL, MessageType.text);
+        }
+        else {
+            const pow = '*Powered by WhatsAsena*'
+            const payload = Config.ALIVEMSG
+            const status = await message.client.getStatus()
+            const ppUrl = await message.client.getProfilePicture() 
+            const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
+
+            if (!payload.includes('{pp}')) {
+                await message.client.sendMessage(message.jid,payload.replace('{version}', Config.VERSION).replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow, MessageType.text);
+            }
+            else if (payload.includes('{pp}')) {
+                await message.sendMessage(Buffer(resim.data), MessageType.image, { caption: payload.replace('{version}', Config.VERSION).replace('{pp}', '').replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow });
+            }
+        }
+    }));
+
+    Asena.addCommand({pattern: 'sysd', fromMe: true, desc: Lang.SYSD_DESC, dontAddCommandList: true }, (async (message, match) => {
 
         const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
         await message.sendMessage(
