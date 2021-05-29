f0536d03a52a6701a6293f0eafbd12491e047cf8
Phoenixx1202 <mcnet.fenix@gmail.com>
2021-05-29 17:54:32 -0400

    Add files via upload

--- plugins/help.js
+++ plugins/help.js
@@ -10,8 +10,8 @@ const Config = require('../config');
 // ==================== MAIN DESCRIPTION TEXT ====================
 const h_Des = "Yardım menüsünden botu kullanım hakkında bilgi verir."
 const h_DedEN = "Dá informações sobre como usar o bot no menu Ajuda."
-const matchnullEN = "==== *🆘 Ajuda Geral 🆘* ====\n\n🔹 *.status:* Verifica se o bot está rodando.\n\n🔹 *.menu:* Mostra a lista completa de comandos.\n\n🔹 *.setvar:* Ele define a configuração sem entrar no Heroku.\n\n Para mais ajuda, use o comando .help \n\n========== *Até mais* =========="
-const matchnull = "========== *🆘 Genel Yardım 🆘* ==========\n\n🔹 *.status:* Botun çalışıp çalışmadığını kontrol eder.\n🔹 *.menu:* Tüm komut listesini gösterir.\n🔹 *.setvar:* Herokuya girmeden config ayarlar.\n\n🔸 Daha fazla yardım için ```.help <yardım almak istediğiniz konu>``` komutunu kullanın.\nÖrnek: ```.help botumu nasıl public yaparım?```\n\n========== *Genel Yardım Bitti* =========="
+const matchnullEN = "==== *🆘 Ajuda Geral 🆘* ====\n\n🔹 *.status:* Verifica se o bot está rodando.\n\n🔹 *.menu:* Mostra a lista completa de comandos.\n\n🔹 *.setvar:* Ele define a configuração sem entrar no Heroku.\n\n Para mais ajuda, use o comando .ajuda \n\n========== *Até mais* =========="
+const matchnull = "========== *🆘 Genel Yardım 🆘* ==========\n\n🔹 *.status:* Botun çalışıp çalışmadığını kontrol eder.\n🔹 *.menu:* Tüm komut listesini gösterir.\n🔹 *.setvar:* Herokuya girmeden config ayarlar.\n\n🔸 Daha fazla yardım için ```.ajuda <yardım almak istediğiniz konu>``` komutunu kullanın.\nÖrnek: ```.ajuda botumu nasıl public yaparım?```\n\n========== *Genel Yardım Bitti* =========="
 const notfound = "```Almak istediğiniz yardım bulunamadı!```\n```Lütfen daha açıklayıcı bir şekilde sorunu belirtin.```"
 const notfoundEN = "```A ajuda que você queria obter não foi encontrada!```\n```Descreva o problema de uma forma mais descritiva.```"
 
@@ -28,7 +28,7 @@ const sudoEN = "SUDO, Compartilha seu bot com o usuário que você escolher, com
 
 if (Config.LANG == 'TR' || Config.LANG == 'AZ') {
     
-    Asena.addCommand({pattern: 'help ?(.*)', fromMe: true, desc: h_Des}, (async (message, match) => {
+    Asena.addCommand({pattern: 'ajuda ?(.*)', fromMe: true, desc: h_Des}, (async (message, match) => {
 
         if (match[1] === '') {
             return await message.client.sendMessage(
@@ -76,7 +76,7 @@ if (Config.LANG == 'TR' || Config.LANG == 'AZ') {
 }
 else {
     
-    Asena.addCommand({pattern: 'help ?(.*)', fromMe: true, desc: h_DedEN}, (async (message, match) => {
+    Asena.addCommand({pattern: 'ajuda ?(.*)', fromMe: true, desc: h_DedEN}, (async (message, match) => {
 
         if (match[1] === '') {
             return await message.client.sendMessage(
